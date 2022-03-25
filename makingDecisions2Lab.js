e6869be448fb16d8a55cedbec329c305062ca19b
Emmaline <emmalinerose19@gmail.com>
2022-03-25 00:48:00 -0500

    completed one advanced problem

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -173,12 +173,45 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
+switch(letterGrade){
+  case 'B':
+    text = `The student is doing well`;
+    break;
+  case 'A':
+    text = `The student is doing excellently`
+    break;
+  case 'C':
+    text = `The student is doing alright`;
+    break;
+  case 'D':
+    text = `The student is not doing very well`;
+    break;
+  case 'F':
+    text = `The student is failing`;
+    break;
+  default:
+    text = `Not an eligible grade`;
+}
+
+console.log(text)
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, 
+  if the number your for loop is currently on is divisible by 3, console.log 'Dev'. 
+  If the number is divisible by 5, console.log 'mountain'. 
+  If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+  If the number is not divisible by 5 or 3, console.log the number itself. 
+  Hint: Look up the modulo operator.
 
+
+
+
+
+
+
+  
   Your output should look like:
   1
   2
